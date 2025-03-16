e3dd90a1d61e187ae460e1c908f55779af377894
Florence Comajes <renztoygwapo@gmail.com>
2025-03-16 18:15:06 +0800

    Initial Set up

---
+++ components/Stepper.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+const items = [
+  {
+    slot: 'business',
+    title: 'Business Details',
+    icon: 'i-lucide-house'
+  },
+  {
+    slot: 'key',
+    title: 'Key Contacts',
+    icon: 'i-lucide-truck'
+  },
+  {
+    slot: 'profile',
+    title: 'Business Profile',
+  },
+  {
+    slot: 'key',
+    title: 'Business Summary',
+    icon: 'i-lucide-truck'
+  },
+  {
+    slot: 'key',
+    title: 'Declaration Account',
+    icon: 'i-lucide-truck'
+  },
+]
+
+const stepper = useTemplateRef('stepper')
+</script>
+
+<template>
+  <div class="w-full">
+    <UStepper ref="stepper" :items="items">
+      <template #content="{ item }">
+        <Placeholder class="aspect-video">
+          {{ item.title }}
+        </Placeholder>
+      </template>
+    </UStepper>
+
+    <div class="flex gap-2 justify-between mt-4">
+      <UButton
+        leading-icon="i-lucide-arrow-left"
+        :disabled="!stepper?.hasPrev"
+        @click="stepper?.prev()"
+      >
+        Prev
+      </UButton>
+
+      <UButton
+        trailing-icon="i-lucide-arrow-right"
+        :disabled="!stepper?.hasNext"
+        @click="stepper?.next()"
+      >
+        Next
+      </UButton>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <UContainer class="mt-16">
+        <Stepper />
+    </UContainer>
+</template>
