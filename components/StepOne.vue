881523710cbca8c64f30b6d3d44118f27a262b28
Florence Comajes <renztoygwapo@gmail.com>
2025-03-17 09:42:47 +0800

    Add More Fields

--- components/StepOne.vue
+++ components/StepOne.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
-import { object, string, type InferType } from 'yup'
+import { boolean, object, string, type InferType } from 'yup'
 import type { FormSubmitEvent } from '@nuxt/ui'
+import { CalendarDate, DateFormatter, getLocalTimeZone } from '@internationalized/date'
+
+
+const status = ref(true)
+const df = new DateFormatter('en-US', {
+  dateStyle: 'medium'
+})
+// const registration_date = shallowRef(new CalendarDate(2025, 3, 18))
+const expiry_date = shallowRef(new CalendarDate(2025, 3, 18))
+const renewal_date = shallowRef(new CalendarDate(2025, 3, 18))
 
 const schema = object({
-  email: string().email('Invalid email').required('Required'),
-  password: string()
-    .min(8, 'Must be at least 8 characters')
-    .required('Required')
+  is_premium: string().required('This Field Required'),
+  business_name: string().required('Business Name is Required'),
+  registration_date: string().required('Registration Date is Required'),
+  renewal_date: string().required('Renewal Date is Required'),
+  expiry_date: string().required('Expiration Date is Required'),
 })
 
 type Schema = InferType<typeof schema>
 
 const state = reactive({
-  email: undefined,
-  password: undefined
+  is_premium: true,
+  business_name: undefined,
+  registration_date: undefined,
+  renewal_date: undefined,
+  expiry_date: undefined,
 })
 
 const toast = useToast()
@@ -24,17 +38,57 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
 </script>
 
 <template>
-  <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-    <UFormField label="Email" name="email">
-      <UInput v-model="state.email" />
+  <section class="flex justify-center w-full">
+    <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+      <UFormField label="Business Name" name="business_name" required>
+        <UInput v-model="state.business_name" label="Business Name"/>
+      </UFormField>
+      <UFormField label="Status">
+      <USwitch v-model="status" />
     </UFormField>
-
-    <UFormField label="Password" name="password">
-      <UInput v-model="state.password" type="password" />
+    <UFormField name="is_premium">
+      <UCheckbox v-model="state.is_premium" label="Is Company a Premium Company (with renewal exemption)" />
     </UFormField>
+    <UFormField label="Accreditational Level">
+      <UCheckbox label="Alumni Shield Level 1" />
+      <UCheckbox label="Is Partially Accredited?" />
+    </UFormField>
+      <UFormField label="Registration Date" name="registration_date" required>
+        <UPopover>
+          <UButton color="neutral" variant="subtle" icon="i-lucide-calendar">
+            {{ state.registration_date ? df.format(state.registration_date.toDate(getLocalTimeZone())) : 'Select a date' }}
+          </UButton>
 
+          <template #content>
+            <UCalendar v-model="state.registration_date" class="p-2" />
+          </template>
+        </UPopover>
+      </UFormField>
+      <UFormField label="Expiry Date" name="expiry_date" required>
+        <UPopover>
+          <UButton color="neutral" variant="subtle" icon="i-lucide-calendar">
+            {{ state.expiry_date ? df.format(state.expiry_date.toDate(getLocalTimeZone())) : 'Select a date' }}
+          </UButton>
+
+          <template #content>
+            <UCalendar v-model="state.expiry_date" class="p-2" />
+          </template>
+        </UPopover>
+      </UFormField>
+      <UFormField label="Renewal Date" name="renewal_date" required>
+        <UPopover>
+          <UButton color="neutral" variant="subtle" icon="i-lucide-calendar">
+            {{ state.renewal_date ? df.format(state.renewal_date.toDate(getLocalTimeZone())) : 'Select a date' }}
+          </UButton>
+
+          <template #content>
+            <UCalendar v-model="state.renewal_date" class="p-2" />
+          </template>
+        </UPopover>
+      </UFormField>
     <UButton type="submit">
       Submit
     </UButton>
   </UForm>
+  </section>
 </template>
